a21e2f8ba7f7458e110322bfbdc20e0eb9d6e180
Jyun <njyun666666@gmail.com>
2024-02-11 01:23:42 +0800

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
  */
 export const environment = {
   production: true,
-  apiUrl: "/api/",
+  apiUrl: "https://forms-api.njyunn.com/api/",
   fileSizeLimit_MB: 1,
   password: "xSv6mFrn53GbeEZu",
 };
